1983885acfccfe4ffa010401fd9ef0971bb6c12c
Louis Taylor <louis@kragniz.eu>
2016-10-16 17:18:43 +0100

    Make Member part of the public api

--- etcd3/__init__.py
+++ etcd3/__init__.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import
 from etcd3.client import Etcd3Client
 from etcd3.client import client
 from etcd3.client import Transactions
+from etcd3.members import Member
 
 __author__ = 'Louis Taylor'
 __email__ = 'louis@kragniz.eu'
 __version__ = '0.1.0'
 
-__all__ = ['Etcd3Client', 'client', 'etcdrpc', 'utils', 'Transactions']
+__all__ = ['Etcd3Client', 'client', 'etcdrpc', 'utils', 'Transactions',
+           'Member']
