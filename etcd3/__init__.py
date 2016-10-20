e81c56e1f3b682e0cfffa40851aed817be3b1812
Louis Taylor <louis@kragniz.eu>
2016-10-20 18:55:43 +0100

    Add Lease to public api

--- etcd3/__init__.py
+++ etcd3/__init__.py
@@ -4,6 +4,7 @@ import etcd3.etcdrpc as etcdrpc
 from etcd3.client import Etcd3Client
 from etcd3.client import Transactions
 from etcd3.client import client
+from etcd3.leases import Lease
 from etcd3.members import Member
 
 __author__ = 'Louis Taylor'
@@ -12,6 +13,7 @@ __version__ = '0.2.2'
 
 __all__ = (
     'Etcd3Client',
+    'Lease',
     'Member',
     'Transactions',
     'client',
