9b97573d6aa12df47c780ad0d5e84760549f49d8
mageshgv <mageshgv@gmail.com>
2017-08-18 14:01:54 -0700

    Raise an exception when the etcd revision being watched is compacted
    Currently in such a scenario, the watcher hangs indefinitely.
    With this fix, the caller to catch the new exception and start watching
    from the compacted version with a new instance of the client.

--- etcd3/exceptions.py
+++ etcd3/exceptions.py
@@ -20,3 +20,9 @@ class ConnectionTimeoutError(Etcd3Exception):
 
 class PreconditionFailedError(Etcd3Exception):
     pass
+
+
+class RevisionCompactedError(Etcd3Exception):
+    def __init__(self, compacted_revision):
+        self.compacted_revision = compacted_revision
+        super(RevisionCompactedError, self).__init__()
