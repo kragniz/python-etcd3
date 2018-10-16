51123096670e5d1d202ab89efc9053874c3b025f
boolean5 <georgia@arrikto.com>
2018-10-16 05:31:07 -0400

    Support range conditions in transactions
    Enable support for transaction conditions that refer to a range of keys.

--- etcd3/transactions.py
+++ etcd3/transactions.py
@@ -10,8 +10,9 @@ _OPERATORS = {
 
 
 class BaseCompare(object):
-    def __init__(self, key):
+    def __init__(self, key, range_end=None):
         self.key = key
+        self.range_end = range_end
         self.value = None
         self.op = None
 
@@ -38,13 +39,19 @@ class BaseCompare(object):
         return self
 
     def __repr__(self):
-        return "{}: {} {} '{}'".format(self.__class__, self.key,
+        if self.range_end is None:
+            keys = self.key
+        else:
+            keys = "[{}, {})".format(self.key, self.range_end)
+        return "{}: {} {} '{}'".format(self.__class__, keys,
                                        _OPERATORS.get(self.op),
                                        self.value)
 
     def build_message(self):
         compare = etcdrpc.Compare()
         compare.key = utils.to_bytes(self.key)
+        if self.range_end is not None:
+            compare.range_end = utils.to_bytes(self.range_end)
 
         if self.op is None:
             raise ValueError('op must be one of =, !=, < or >')
