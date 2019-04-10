21189aa068652e2b7874257ced27e8ecacdb19df
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2019-04-10 17:07:08 -0400

    export Etcd3Exception

--- etcd3/__init__.py
+++ etcd3/__init__.py
@@ -4,6 +4,7 @@ import etcd3.etcdrpc as etcdrpc
 from etcd3.client import Etcd3Client
 from etcd3.client import Transactions
 from etcd3.client import client
+from etcd3.exceptions import Etcd3Exception
 from etcd3.leases import Lease
 from etcd3.locks import Lock
 from etcd3.members import Member
@@ -15,6 +16,7 @@ __version__ = '0.9.0'
 __all__ = (
     'etcdrpc',
     'Etcd3Client',
+    'Etcd3Exception',
     'Transactions',
     'client',
     'Lease',
